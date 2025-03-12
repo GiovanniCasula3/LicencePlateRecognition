bc2f98f8fb224d668faf254de30635746dc54dc9
Giovanni Casula <giovannicasula2003@gmail.com>
2025-03-12 12:34:50 +0100

    terminato segment_marging

--- segment_magging.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from skimage.io import resize
-from skimage import measure
-from skimage.measure import regionprops
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import connected_regions
-
